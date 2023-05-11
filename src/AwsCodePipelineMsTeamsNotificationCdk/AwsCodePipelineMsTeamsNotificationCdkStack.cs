28c4631b936850c1dd0281e7134ba22161a67154
Archie Austria <archie.austria@radix.com.au>
2023-05-11 20:49:40 +0800

    Initial commit

---
+++ src/AwsCodePipelineMsTeamsNotificationCdk/AwsCodePipelineMsTeamsNotificationCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCodePipelineMsTeamsNotificationCdk
+{
+    public class AwsCodePipelineMsTeamsNotificationCdkStack : Stack
+    {
+        internal AwsCodePipelineMsTeamsNotificationCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
